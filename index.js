b2f63a4bcb49f42e192bab9395352dc439344f4e
rubenbenitez <rubendariorolonbenitez@gmail.com>
2022-08-25 15:03:32 -0400

    Se arreglaron los espacios

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ let nombre = "juan perez";
 let edad = calcularEdad(2002);
 console.log("hola"+ nombre);
 console.log(nombre + "tiene"+ edad+"años");
-
 console.log("se le pagara:"+calcularsalario(8,30000));
+
  function calcularEdad(añonacimiento){
     return new Date().getFullYear() -añonacimiento;
 }
+
 function calcularsalario(horastrabajadas, montoporhora){
     return horastrabajadas * montoporhora;
 }
