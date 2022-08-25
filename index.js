31ee4a2bcb42d89806c30a0c904caef5016c7148
rubenbenitez <rubendariorolonbenitez@gmail.com>
2022-08-25 15:02:28 -0400

    Se creo la funcion calcularSalario

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ let nombre = "juan perez";
 let edad = calcularEdad(2002);
 console.log("hola"+ nombre);
 console.log(nombre + "tiene"+ edad+"años");
+console.log("Se va a pagar: "+calcularSalario(16));
+
 function calcularEdad(añonacimiento){
     return new Date().getFullYear() -añonacimiento;
+}
+
+function calcularSalario(horasTrabajadas){
+    return horasTrabajadas * 20000;
 }
